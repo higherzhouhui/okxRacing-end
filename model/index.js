87c97f27f90357ede2235ca7e633fb85ee0e8cd4
ahui <782492184@qq.com>
2024-11-05 11:58:26 +0800

    update

--- model/index.js
+++ model/index.js
@@ -97,7 +97,7 @@ const Config = db.sequelize.define(
     invite_friends_score: { type: DataTypes.INTEGER, defaultValue: 5000 },
     recovery_time: { type: DataTypes.INTEGER, defaultValue: 90 },
     bind_wallet_score: { type: DataTypes.INTEGER, defaultValue: 0 },
-    tg_link: { type: DataTypes.STRING, defaultValue: 'https://t.me/quickswitchtestbot' },
+    tg_link: { type: DataTypes.STRING, defaultValue: 'https://t.me/quickswitchtestbot/quickswitch' },
   },
   {
     tableName: 'config'
